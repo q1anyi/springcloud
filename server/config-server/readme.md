36bfcd3f1355c02f456de9f8508890e9d67db0ac
caicy <971031618@qq.com>
2019-07-14 15:43:29 +0800

    搭建config-server

---
+++ server/config-server/readme.md
@@ -0,0 +1,1 @@
+# Spring-Cloud-Config统一配置管理
---
+++ server/config-server/src/main/java/com/springcloud/ConfigApplication.java
@@ -0,0 +1,18 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: caicy
+ * @Date: Created In 2019-07-01 23:34
+ * @Description:
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
